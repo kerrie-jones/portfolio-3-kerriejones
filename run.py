a1926db2b3e022378d0ca7e9cb772b5759cdb151
kerrie-jones <kerriejones81@icloud.com>
2023-01-20 19:31:33 +0000

    Add validation to check name is only letters, staff number is 8 numerical digits

--- run.py
+++ run.py
@@ -15,12 +15,48 @@ SHEET = GSPREAD_CLIENT.open('EQ6453 Intake Forms')
 
 def login():
     """
-    get username input from user. 
-    Then bring user to main menu
+    get username and staff number input from user.
+    error message will appear if data invalid
+    If valid will bring user to main menu
     """
-    name = input("Enter your name and surname: ")
-    print(f"Hello {name}")
-    input("Enter your 8 digit staff number: ")
-    print(f"Loading main menu...")
+    print("Please enter your name and staff number below")
+
+    name = input("Enter your name: ")
+    staff_number = input("Enter your 8 digit staff number: ")
+
+    validate_login(name, staff_number)
+
+
+def validate_login(name, staff_number):
+    """
+    Raises value error if name is not letter.
+    Raises ValueError if there arent exactly 8 values in staffnumber
+    Raises ValueError if staffnumber is not numeric
+    """
+    try:
+        if not name.isalpha():
+            raise ValueError(
+                "no symbols or numbers allowed in name"
+                )
+        if not staff_number.isnumeric():
+            raise ValueError(
+                "non numeric value entered in staff number"
+                )
+        if len(staff_number) != 8:
+            raise ValueError(
+                f"8 digits required for staff no. {len(staff_number)} entered"
+                )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again\n")
+
+    # )
+    # print(f"Hello {name}")
+    # if len(staff_number) != 8:
+    #     raise ValueError("invalid data please enter 8 digit number")
+    # if not staff_number.isnumeric():
+    #     raise ValueError("invalid data please enter numerical value only")
+
+    # print("Loading main menu...")
+
 
 login()
