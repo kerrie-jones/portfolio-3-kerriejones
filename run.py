095acb20f7a992d56c3e93388a16865c158de7f9
kerrie-jones <kerriejones81@icloud.com>
2023-01-20 15:50:18 +0000

    Add login function

--- run.py
+++ run.py
@@ -12,8 +12,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('EQ6453 Intake Forms')
 
-forms = SHEET.worksheet('forms')
 
-data = forms.get_all_values()
+def login():
+    """
+    get username input from user. 
+    Then bring user to main menu
+    """
+    name = input("Enter your name and surname: ")
+    print(f"Hello {name}")
+    input("Enter your 8 digit staff number: ")
+    print(f"Loading main menu...")
 
-print(data)
+login()
